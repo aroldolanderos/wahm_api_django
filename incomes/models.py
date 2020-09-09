64a9c5e3b9aafd20e185e6703290fd7a9472ec54
Aroldo Landeros <aroldolanderos@gmail.com>
2020-09-09 19:57:06 -0300

    formateo de código

--- incomes/models.py
+++ incomes/models.py
@@ -9,10 +9,19 @@ from django.utils import timezone
 
 class Income(models.Model):
     """Incomes model."""
-    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    uuid = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
     description = models.TextField(max_length=50, blank=False)
     amount = models.PositiveIntegerField(default=0)
-    created_at = models.DateTimeField(default=timezone.now, editable=False, null=False, blank=False)
+    created_at = models.DateTimeField(
+        default=timezone.now,
+        editable=False,
+        null=False,
+        blank=False
+    )
 
     class Meta:
         """Meta option."""
